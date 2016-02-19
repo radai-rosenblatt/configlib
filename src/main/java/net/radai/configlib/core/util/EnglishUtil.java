62739c6ff360fc3276af711dd998d436fb10e1c6
radai-rosenblatt <radai.rosenblatt@gmail.com>
2016-02-19 14:22:27 -0800

    fix up copyright formatting

--- src/main/java/net/radai/configlib/core/util/EnglishUtil.java
+++ src/main/java/net/radai/configlib/core/util/EnglishUtil.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with ConfigLib.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package net.radai.configlib.core.util;
 
 import java.util.Locale;
--- src/test/java/net/radai/configlib/core/cats/Cat.java
+++ src/test/java/net/radai/configlib/core/cats/Cat.java
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with ConfigLib.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package net.radai.configlib.core.cats;
 
 import java.util.List;
